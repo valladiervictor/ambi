8e963dce245c3af2b6827e80be31d6dcb84e4174
victorvalladier <victorvalladier@gmail.com>
2017-07-25 12:05:36 +0200

    Resize thumbnails on small screens

--- ambi/app/assets/javascripts/room_show.js
+++ ambi/app/assets/javascripts/room_show.js
@@ -1,3 +1,15 @@
-function emptyNewSongForm(song_id) {
+function emptyNewSongForm() {
   $("#new-song-txt-field").val("")
+}
+
+function sizeThumbnails() {
+  width = 200
+  while(width > $(window).width() / 3) {
+    width -= 10
+  }
+  thumbnails = document.querySelectorAll('.thumbnail')
+  thumbnailArray = Array.prototype.slice.call(thumbnails);
+  thumbnailArray.forEach(function (img) {
+    img.width = width
+  });
 }
