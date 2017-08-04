9beb17d6bf15e9e1ef59b227c00c51dc67a66546
victorvalladier <victorvalladier@gmail.com>
2017-08-04 19:35:54 +0200

    wip

--- ambi/app/controllers/songs_controller.rb
+++ ambi/app/controllers/songs_controller.rb
@@ -10,42 +10,47 @@ class SongsController < ApplicationController
       end
     else
       @player = Player.find(@room.player_id)
-      @room.update modified_at: DateTime.now
-      @player.update modified_at: DateTime.now
+      @room.update modified_at: DateTime.now.to_i
+      @player.update modified_at: DateTime.now.to_i
 
       link = parseLink(params["song"]["link"])
 
       is_current = @player.song_id ? false : true
 
       video = Yt::Video.new id: link
-      if !video
+
+      begin
+        @song = Song.new(
+          name: video.title,
+          link: link,
+          room_id: params["room_id"],
+          poll: 1,
+          thumbnail: "https://img.youtube.com/vi/#{link}/mqdefault.jpg",
+          modified_at: DateTime.now.to_i,
+          past: false,
+          liked: false
+        )
+        @song.save
+
+        if (is_current)
+          @player.update song_id: @song.id
+          @song.update past: true
+          sync_update @player
+        else
+          @room.reload
+          byebug
+          sync_update @room
+        end
+
+        respond_to do |format|
+          format.json { render json: [], status: 200 }
+          format.html { redirect_to @room }
+        end
+      rescue
         respond_to do |format|
           format.json { render status: 400 }
           format.html { redirect_to @room }
         end
-      end
-
-      @song = Song.new(
-        name: video.title,
-        link: link,
-        room_id: is_current ? nil : params["room_id"],
-        poll: 1,
-        thumbnail: "https://img.youtube.com/vi/#{link}/mqdefault.jpg",
-        modified_at: DateTime.now
-      )
-      @song.save
-
-      if (is_current)
-        @player.update song_id: @song.id
-        sync_update @player
-      else
-        @room.reload
-        sync_update @room
-      end
-
-      respond_to do |format|
-        format.json { render json: [], status: 200 }
-        format.html { redirect_to @room }
       end
     end
   end
@@ -58,13 +63,17 @@ class SongsController < ApplicationController
     @song = Song.find(params["id"])
     @room = Room.find(@song.room_id)
 
-    if (params["poll"].blank?)
+    if (params["poll"].blank? && paramas["liked"].blank? && params["past"].blank?)
       render status: 400
     end
-    if (session[@room.id.to_s + '/' + @song.id.to_s] != params["poll"])
-      poll = params["poll"] == "minus" ? -1 : 1
-      @song.update poll: @song.poll + poll
-      session[@room.id.to_s + '/' + @song.id.to_s] = params["poll"]
+    if (params.has_key?(:poll))
+      if (session[@room.id.to_s + '/' + @song.id.to_s] != params["poll"])
+        poll = params["poll"] == "minus" ? -1 : 1
+        @song.update poll: @song.poll + poll
+        session[@room.id.to_s + '/' + @song.id.to_s] = params["poll"]
+      end
+    elsif (params.has_key?(:liked))
+      @song.update liked: true
     end
     @room.reload
     sync_update @room
